045188d3cde2e8dacd2d5378a401f4cab18c189a
suab321 <adityasaha890@gmail.com>
2018-09-16 22:06:02 +0530

    as

---
+++ routes/order.js
@@ -0,0 +1,58 @@
+const router=require('express').Router()
+const {user_model}=require('../database/data_base')
+const bodyparser=require('body-parser')
+
+router.use(bodyparser.urlencoded({extended:false}))
+router.use(bodyparser.json())
+
+//adding items to cart
+router.put('/add',(req,res)=>{
+    user_model.findOneAndUpdate({email:req.body.email},
+        {$addToSet:{'cart':{'name':req.body.name,'price':req.body.price,
+        'date':Date.now(),'category':req.body.category,'url':req.body.url}}})
+        .then(user=>res.status(200).json(user))
+        .catch(err=>res.status(400).json(err))
+})
+//removing items from cart
+router.delete('/remove/:email/:id',(req,res)=>{
+    user_model.findOneAndUpdate({email:req.params.email},{$pull:{'cart':{'_id':req.params.id}}})
+    .then(user=>res.status(200).json(user[0].order))
+    .catch(err=>res.status(400).json(err))
+})
+
+//getting food data from cart
+router.get('/cart_food/:email',(req,res)=>{
+    user_model.find({email:req.params.email},{cart:true}).then(user=>{
+        res.status(200).json(user[0].cart)
+    })
+})
+
+//adding placed orders
+router.put('/order_placed/:email',(req,res)=>{
+    user_model.findOneAndUpdate({email:req.params.email},
+        {$addToSet:{'order_placed'
+        :{'orders'
+        :{'name':req.body.name,'price':req.body.price,'category':req.body.category,'url':req.body.url},date:Date.now()}}
+    }).then(user=>res.status(200).json(user))
+})
+//getting placed orders from db
+router.get('/get_placed_order/:email',(req,res)=>{
+    user_model.find({email:req.params.email},{order_placed:true})
+    .then(user=>res.status(200).json(user[0].order_placed))
+    .catch(err=>res.status(500).json(err))
+})
+//clearing cart
+router.delete('/delete_cart/:email',(req,res)=>{
+    user_model.find({email:req.params.email},{cart:true}).then(
+        user=>{
+            //res.json(user[0].cart)
+            user[0].cart.map(item=>{
+                user_model.findOneAndUpdate({email:req.params.email},{$pull:{'cart':{'name':item.name}}})
+                .then(user=>res.status(200).json(user))
+                .catch(err=>res.json(err))
+            })
+        }
+    )
+})
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,100 @@
+const express=require('express')
+const bodyparser=require('body-parser')
+const mongoose=require('mongoose')
+const bcrypt=require('bcrypt')
+const session=require('express-session')
+const {user_model}=require('./database/data_base')
+const {food_model}=require('./database/data_base')
+const orders=require('./routes/order')
+const cors=require('cors')
+const cookieparser=require('cookie-parser')
+
+
+const app=express()
+app.use(cookieparser())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+       next();
+ });
+app.use(cors({
+    credentials:true,
+    origin: ['http://localhost:3000/index'],
+    methods:['GET','POST','PUT','DELETE']
+}))
+app.use(session({key:'user_sid',
+                secret:'suab',
+                resave:true,
+	            saveUninitialized:true,
+            cookie:{maxAge:null}}))
+
+const check=(req,res,next)=>{
+    if(req.session.user&&req.cookies.user_sid)
+        res.redirect('http://localhost:3000/index')
+    else
+        next()
+}
+//Cart urls
+app.use('/cart',orders);
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+
+app.get('/cookie',(req,res)=>{
+    if(req.session.user && req.cookies.user_sid){
+        res.json(req.session.user.email)
+    }
+})
+//Home
+app.get('/',check,(req,res)=>{
+    res.redirect('http://localhost:3000/Login')
+})
+//Login
+app.get('/get_login',(req,res)=>{res.status(200).redirect('http://localhost:3000/Login')})
+
+app.post('/login',(req,res)=>{
+    const email=req.body.email
+    user_model.findOne({email},{email:true,password:true}).then(user=>{
+        if(bcrypt.compareSync(req.body.password,user.password)){
+            req.session.user=user
+            res.status(200).redirect('/')
+        }
+        else
+            res.redirect('http://localhost:3000/Login')
+    }).catch(err=>{res.status(400).redirect('http://localhost:3000/Login')})
+})
+
+//User Register//
+app.post('/register',(req,res)=>{
+    const db=new user_model
+    db.email=req.body.email
+    db.password=bcrypt.hashSync(req.body.password||req.body.cpassword,10)
+    db.save().then(user=>{
+        req.session.user=user
+        res.status(200).redirect('http://localhost:3000/index')})
+        .catch(err=>{res.status(400).redirect('http://localhost:3000/Register')})
+})
+
+//logout//
+app.get('/logout',(req,res)=>{
+    if(req.session.user && req.cookies.user_sid)
+       res.clearCookie('user_sid').json('ok')
+})
+
+//Loading food
+app.post('/load_food',(req,res)=>{
+    const db=new food_model
+    db.name=req.body.name
+    db.price=req.body.price
+    db.category=req.body.cat
+    db.url=req.body.url
+    db.type=req.body.type
+    db.gene=req.body.gene
+    db.save().then(user=>{res.status(200).json(user)}).catch(err=>res.status(500).json(err))
+})
+//getting food_item
+app.get('/food',(req,res)=>{
+    food_model.find({}).then(user=>{res.status(200).json(user)}).catch(err=>res.json(err))
+})
+
+app.listen(process.env.PORT||3002)
